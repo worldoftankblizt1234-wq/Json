7dd47652e61f5fab54ffda429a0813172e4f4b39
worldoftankblizt1234-wq <worldoftankblizt1234@gmail.com>
2026-08-05 16:53:14 +0700

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,1 +1,23 @@
+// === FIREBASE CONFIG ===
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, ref, set, onValue, push, update, remove } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCGXq3xpUv_qaH5R7RB9LlJwsnVhrlewoA",
+    authDomain: "country-61ecf.firebaseapp.com",
+    databaseURL: "https://country-61ecf-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "country-61ecf",
+    storageBucket: "country-61ecf.firebasestorage.app",
+    messagingSenderId: "91679803947",
+    appId: "1:91679803947:web:4700703e957e9c1b1cb86e",
+    measurementId: "G-XQFVPJLF7R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const analytics = getAnalytics(app);
+
+// Export
+export { database, ref, set, onValue, push, update, remove };
